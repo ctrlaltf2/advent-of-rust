9a1d08797060c9fc94f8a29defe4e1bf308f0fc1
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-12-04 11:38:40 -0500

    Day 1 Part 2

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -1,17 +1,44 @@
+use std::collections::HashSet;
+use std::env;
 use std::io;
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut part2 = false;
+    for arg in &args {
+        if arg == "part2" {
+            part2 = true;
+        }
+    }
+
     let mut ln = String::new();
-    let mut sum = 0;
+    let mut delta_freqs: Vec<i32> = Vec::new();
     loop {
         ln.clear();
         io::stdin().read_line(&mut ln)?;
+
         if ln.trim() == "" {
             break;
-        } else {
-            sum += ln.trim().parse::<i32>().unwrap();
+        }
+
+        delta_freqs.push(ln.trim().parse::<i32>().unwrap());
+    }
+
+    if !part2 {
+        let sum: i32 = delta_freqs.iter().sum();
+        println!("Part 1: {}", sum);
+        return Ok(());
+    } else {
+        let mut freq = 0;
+        let mut set = HashSet::new();
+        loop {
+            for x in &delta_freqs {
+                if !set.insert(freq) {
+                    println!("Part 2: {}", freq);
+                    return Ok(());
+                }
+                freq += x;
+            }
         }
     }
-    println!("'{}'", sum);
-    Ok(())
 }
