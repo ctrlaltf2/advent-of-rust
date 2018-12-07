c9be339ceb1bfcf98743e5357239696011e0b89e
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-12-07 08:25:36 -0500

    Day 2 Part 1 Refactor

--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io;
+use std::io::Read;
 use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
@@ -11,26 +12,19 @@ fn main() -> io::Result<()> {
         }
     }
 
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
     let (mut twos, mut threes) = (0, 0);
-    loop {
-        let mut ln = String::new();
-        io::stdin().read_line(&mut ln)?;
-        
-        ln = ln.trim().to_string();
-
-        if ln == "" {
-            break;
-        }
-
+    for line in input.lines() {
         let mut chars: HashMap<char, u32> = HashMap::new();
         
-        for ch in ln.chars() {
+        for ch in line.chars() {
             let count = chars.entry(ch).or_insert(0);
             *count += 1;
         }
 
         let (mut seen_two, mut seen_three) = (false, false);
-
         for (_, count) in chars {
             if count == 2 && !seen_two {
                 twos += 1;
@@ -43,7 +37,6 @@ fn main() -> io::Result<()> {
             }
         }
     }
-
     println!("Part 1: {}", threes * twos);
 
     Ok(())
