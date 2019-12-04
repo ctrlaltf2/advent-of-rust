66908c7eddc02ef163a43008cb76eb296c793c8a
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2019-12-04 15:46:08 -0500

    2019 Day 01 Part 2

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,6 +1,14 @@
+use std::env;
 use std::fs;
 
 fn main() {
+    let mut part2: bool = false;
+    for arg in env::args() {
+        if arg == "--part2" {
+            part2 = true;
+        }
+    }
+
     let contents = fs::read_to_string("./input").expect("Something happened :(");
     let masses = contents.split('\n');
     let mut fuel_requirement: i32 = 0;
@@ -10,8 +18,18 @@ fn main() {
         }
         let mass_num = m.trim().parse::<i32>().unwrap();
 
-        fuel_requirement += mass_num / 3;
-        fuel_requirement -= 2;
+        let mut fuel_req = mass_num / 3 - 2;
+
+        if part2 {
+            while fuel_req > 0 {
+                fuel_requirement += fuel_req;
+
+                fuel_req /= 3;
+                fuel_req -= 2;
+            }
+        } else {
+            fuel_requirement += fuel_req
+        }
     }
 
     println!("Sum of the fuel requirements: {}", fuel_requirement);
