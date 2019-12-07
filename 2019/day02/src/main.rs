24bfe340bcc76d77d6559555d17b36d98a29268c
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2019-12-07 01:17:46 -0500

    2019 Day 02 Part 2

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -1,43 +1,68 @@
+use std::env;
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input").expect("Something happened :(");
-    let mut opcodes: Vec<i32> = Vec::new();
-    for s in input.split(",") {
-        if s.len() == 0 {
-            continue;
+    let mut part2: bool = false;
+    for arg in env::args() {
+        if arg == "--part2" {
+            part2 = true;
         }
-        opcodes.push(s.trim().parse::<i32>().unwrap());
     }
 
-    // replace position 1 with the value 12 and replace position 2 with the value 2
-    opcodes[1] = 12;
-    opcodes[2] = 2;
-
-    let mut halted: bool = false;
-    let mut i: usize = 0;
-    while !halted {
-        match opcodes[i] {
-            // add, a, b, dst
-            1 => {
-                let a = opcodes[i + 1] as usize;
-                let b = opcodes[i + 2] as usize;
-                let dst = opcodes[i + 3] as usize;
-                opcodes[dst] = opcodes[a] + opcodes[b];
-            }
-            // mul, a, b, dst
-            2 => {
-                let a = opcodes[i + 1] as usize;
-                let b = opcodes[i + 2] as usize;
-                let dst = opcodes[i + 3] as usize;
-                opcodes[dst] = opcodes[a] * opcodes[b];
-            }
-            99 => halted = true,
-            _ => println!("Warning: Invalid opcode encountered, i = {}", i),
-        }
-
-        i += 4;
+    fn parse(s: &str) -> u32 {
+        s.trim().parse::<u32>().unwrap()
     }
 
-    println!("{}", opcodes[0]);
+    let program: Vec<u32> = input.split(',').map(parse).collect();
+    let mut memory = program.clone();
+
+    let (mut noun, mut verb): (u32, u32) = (0, 0);
+    if !part2 {
+        // Part 1 inputs
+        noun = 12;
+        verb = 2;
+    }
+
+    while noun != std::u32::MAX {
+        memory[1] = noun;
+        memory[2] = verb;
+        let mut i: usize = 0;
+        while memory[i] != 99 {
+            let a = memory[i + 1] as usize;
+            let b = memory[i + 2] as usize;
+            let dst = memory[i + 3] as usize;
+
+            match memory[i] {
+                1 => memory[dst] = memory[a] + memory[b],
+                2 => memory[dst] = memory[a] * memory[b],
+                _ => println!("Warning: Invalid opcode encountered, i = {}", i),
+            }
+
+            i += 4;
+        }
+
+        if !part2 {
+            println!("Value a position 0 after halting: {}", memory[0]);
+            break;
+        }
+
+        if memory[0] == 19690720 {
+            // Part 2 goal!
+            println!("The pair of inputs to produce 19690720 was noun = {}, verb = {}. 100 * noun + verb = {}", noun, verb, 100 * noun + verb);
+            break;
+        }
+
+        // Reset memory
+        memory = program.clone();
+
+        // Increment inputs
+        // noun ranges from 0...u32::MAX, verb ranges from 0...noun
+        if noun == verb {
+            noun += 1;
+            verb = 0;
+        } else {
+            verb += 1;
+        }
+    }
 }
